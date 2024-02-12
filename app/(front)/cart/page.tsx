7b4cddb54750690b67528ec386b933f25a94c177
PrinceNanaPoku <princenanapoku64@gmail.com>
2024-02-12 15:20:36 +0000

    implemented add to cart page

---
+++ app/(front)/cart/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartDetails from "./cart_details";
+import { title } from "process";
+
+const metadata = {
+  title: "Shopping Cart",
+};
+const CartPage = () => {
+  return <CartDetails />;
+};
+
+export default CartPage;
